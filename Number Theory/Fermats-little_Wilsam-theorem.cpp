990c376d1b5a7c8da36ba0b882c64b0e56fe28ee
Roshan1115 <roshanbishi7890@gamil.com>
2021-11-22 12:18:48 +0530

    number therory challenges and theorem

---
+++ Number Theory/Fermats-little_Wilsam-theorem.cpp
@@ -0,0 +1,29 @@
+/*
+fermat little theorem
+if p is a prime number then for any integer a, then number a^p - a is the multiple of p
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<set>
+#include<limits>
+using namespace std;
+
+#define endline cout<<endl
+#define vi vector<int>
+#define vii vector<pair<int, int> >
+#define pii pair<int, int>
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+int main(){
+  
+  return 0;
+}
+
+// use : g++ -std=c++11 
